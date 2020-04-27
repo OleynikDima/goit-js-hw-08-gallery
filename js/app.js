59948c2a2f34b424f47b169a546b1a2a1857a96f
Dima Oleynik <dimaoleynik@MacBook-Pro-Dima.local>
2020-04-27 13:03:54 +0300

    add pressKey and change modalDelegation

--- js/app.js
+++ js/app.js
@@ -23,47 +23,69 @@ const createGallery = galleryItems.map((el)=>{
 }).join('');
 
 //Listeners
+
 gallery.insertAdjacentHTML('beforeend', createGallery);
 gallery.addEventListener('click', handClick);
-button.addEventListener('click', closeModal);
 //дополнительное задания -- закрываем по клику
 divModal.addEventListener('click', closeModal);
 
-// // Создаем класс и задаем src = путь
+
+
+///// Проходит делегирования при target = createMaldWin
 function handClick(e){
     e.preventDefault();
     if(e.target === e.currentTarget ){
         return;
     }
+    const dataSource = e.target.dataset.source
+    createModalWin(dataSource);   
+}
+// Создаем класс и задаем src = путь
+// создаем отдельно Модальное окно (облегчить код)
+function createModalWin(src){
     divModal.classList.add('is-open')
-    boxImg.src = e.target.dataset.source
-    document.addEventListener('keydown', closeEsc)
+    boxImg.src = src
+    boxImg.transi
+    document.addEventListener('keydown', diferentKeyPress)
 }
 
-/// закрываем модальное окно 
+/// закрываем модальное окно // Делегирование закрытия модалОкна кроме картинки
 function closeModal(e){
+    e.preventDefault();
     if (e && e.target === boxImg){
         return;
     }
     divModal.classList.remove('is-open');
     boxImg.src = '';
-    document.removeEventListener('keydown', closeEsc)
+    document.removeEventListener('keydown', diferentKeyPress)
 }
 
-/// Esc 
-//// перелистывая картинцки leafingЕhrough
-
-function closeEsc(e){
+function diferentKeyPress(e){
+    
+    /// Esc 
     const code = e.code
-    if (code === "Escape"){ 
-        closeModal();
-    }else if(code === "ArrowRight"){
-        let find = galleryItems.find((el,ind)=> el.original === e.target.href)
-        if(find === galleryItems.length){
-           
+     if (code === "Escape"){ 
+         closeModal(e);
+
+    //// перелистывая картинцки leafingЕhrough
+        }else if(code === "ArrowRight"){
+        
+            const nImg = galleryItems.findIndex((el) => el.original === boxImg.src);
+            let sum = nImg+1;
+            if(sum >= galleryItems.length){
+                sum = 0;
+            }
+            createModalWin(galleryItems[sum].original);
+         }else if(code === "ArrowLeft"){
+        
+            const nImg = galleryItems.findIndex((el) => el.original === boxImg.src);
+            let sum = nImg-1;
+            // console.log(sum);
+            if(sum < 0){
+                sum = galleryItems.length-1;
+            }
+            createModalWin(galleryItems[sum].original);
         }
-        console.log(find);
-    }
 }
 
 
